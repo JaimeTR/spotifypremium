2fb3739189dacb72700ba2a9ab24fab8b6ea6f67
Jaime Tarazona <38092883+JaimeTR@users.noreply.github.com>
2021-10-04 11:28:34 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,62 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/31l6ynm0a1fhr2vs/branch/master?svg=true)](https://ci.appveyor.com/project/mrpond/blockthespot/branch/master)  [![Discord](https://discord.com/api/guilds/807273906872123412/widget.png)](https://discord.gg/p43cusgUPm)
+
+<center>
+    <h3 align="center"><a href="https://en.wikipedia.org/wiki/2020_Thai_protests">Support the 2020 Thailand & 2021 Myanmar protests</a></h3>
+    <h3 align="center">Help me stop the dictatorship in Thailand and Myanmar!</h3>
+    <h3 align="center">Southeast Asia strong together!</h3>
+</center>
+
+<center>
+    <h1 align="center">BlockTheSpot</h1>
+    <h4 align="center">A multi-purpose adblocker and skip-bypass for the <strong>Windows</strong> Spotify desktop application.</h4>
+    <h5 align="center">Please support Spotify by purchasing premium</h5>
+    <p align="center">
+        <strong>Last updated:</strong> 17 August 2021<br>
+        <strong>Last tested version:</strong> 1.1.68.628.geb44bd66
+    </p> 
+</center>
+
+#### Important checks before installing:
+0. Update Windows, update Spotify and update BlockTheSpot
+1. Go to "Windows Security" -> "Virus & Threat Protection"
+2. Click "Allowed threats" -> "Remove all allowed threats"
+
+### Features:
+* Blocks all banner/video/audio ads within the app
+* Retains friend, vertical video and radio functionality
+* Unlocks the skip function for any track
+* Now supports the new Alpha version (New UI)
+
+:warning: This mod is for the [**Desktop Application**](https://www.spotify.com/download/windows/) of Spotify on Windows only and **not the Microsoft Store version**.
+
+### Installation/Update:
+* Just download and run [BlockTheSpot.bat](https://raw.githack.com/mrpond/BlockTheSpot/master/BlockTheSpot.bat)  
+
+or
+
+* Run The following command in PowerShell:
+```ps1
+Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression
+```
+
+or
+
+1. Browse to your Spotify installation folder `%APPDATA%\Spotify`
+2. Download `chrome_elf.zip` from [releases](https://github.com/mrpond/BlockTheSpot/releases)
+3. Unzip and replace `chrome_elf.dll` and `config.ini` 
+
+### Uninstall:
+* Just run uninstall.bat
+or
+* Reinstall Spotify 
+
+### Known Issues:  
+* You may face issue [#150](https://github.com/mrpond/BlockTheSpot/issues/150). Can be fixed by enabling the experimental feature when using `BlockTheSpot.bat`.    
+* We support last 2 version of Spotify (latest + previous) only. Please check it before opening an issue.
+
+### Additional Notes:  
+* Remove "Upgrade" Button [#83](https://github.com/mrpond/BlockTheSpot/issues/83) and Remove "Ad Placeholder" [#150](https://github.com/mrpond/BlockTheSpot/issues/150) only works when you use any of the auto installation methods and press `y` when prompted.  
+* "chrome_elf.dll" gets replaced by the Spotify installer each time it updates, hence why you'll probably need to apply the patch again when it happens
+* [Spicetify](https://github.com/khanhas/spicetify-cli) users will need to reapply BlockTheSpot after applying a Spicetify patches.
+* If the automatic install/uninstall scripts do not work, please contact [Nuzair46](https://github.com/Nuzair46)
+* For more support and discussions, join our [Discord server](https://discord.gg/p43cusgUPm) 
---
+++ install.ps1
@@ -0,0 +1,221 @@
+# Ignore errors from `Stop-Process`
+$PSDefaultParameterValues['Stop-Process:ErrorAction'] = 'SilentlyContinue'
+
+write-host @'
+***************** 
+@mrpond message:
+#Visitame en instagram y sigueme jaimetr_
+Github: JaimeTR
+***************** 
+'@
+
+write-host @'
+***************** 
+Author: @ JaimeTR
+***************** 
+'@
+
+$SpotifyDirectory = "$env:APPDATA\Spotify"
+$SpotifyExecutable = "$SpotifyDirectory\Spotify.exe"
+$SpotifyApps = "$SpotifyDirectory\Apps"
+
+Write-Host 'Deteniendo Spotify...'`n
+Stop-Process -Name Spotify
+Stop-Process -Name SpotifyWebHelper
+
+if ($PSVersionTable.PSVersion.Major -ge 7)
+{
+    Import-Module Appx -UseWindowsPowerShell
+}
+
+if (Get-AppxPackage -Name SpotifyAB.SpotifyMusic) {
+  Write-Host @'
+Se ha detectado la versión de Spotify de Microsoft Store que no es compatible.
+'@`n
+  $ch = Read-Host -Prompt "Desintalar la version de Spotify de Windows (Y/N) "
+  if ($ch -eq 'y'){
+     Write-Host @'
+Desintalada la version de Spotify de Windows
+'@`n
+     Get-AppxPackage -Name SpotifyAB.SpotifyMusic | Remove-AppxPackage
+  } else{
+     Write-Host @'
+Saliendo....
+'@`n
+     Pause 
+     exit
+    }
+}
+
+Push-Location -LiteralPath $env:TEMP
+try {
+  # Unique directory name based on time
+  New-Item -Type Directory -Name "BlockTheSpot-$(Get-Date -UFormat '%Y-%m-%d_%H-%M-%S')" `
+  | Convert-Path `
+  | Set-Location
+} catch {
+  Write-Output $_
+  Pause
+  exit
+}
+
+Write-Host 'Descargando el utlimo parche (chrome_elf.zip)...'`n
+$webClient = New-Object -TypeName System.Net.WebClient
+try {
+  $webClient.DownloadFile(
+    # Remote file URL
+    'https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip',
+    # Local file path
+    "$PWD\chrome_elf.zip"
+  )
+} catch {
+  Write-Output $_
+  Sleep
+}
+<#
+try {
+  $webClient.DownloadFile(
+    # Remote file URL
+    'https://github.com/mrpond/BlockTheSpot/files/5969916/zlink.zip',
+    # Local file path
+    "$PWD\zlink.zip"
+  )
+} catch {
+  Write-Output $_
+  Sleep
+}
+try {
+  $webClient.DownloadFile(
+    # Remote file URL
+    'https://github.com/mrpond/BlockTheSpot/files/6234124/xpui.zip',
+    # Local file path
+    "$PWD\xpui.zip"
+  )
+} catch {
+  Write-Output $_
+  Sleep
+}
+#>
+Expand-Archive -Force -LiteralPath "$PWD\chrome_elf.zip" -DestinationPath $PWD
+Remove-Item -LiteralPath "$PWD\chrome_elf.zip"
+<#
+Expand-Archive -Force -LiteralPath "$PWD\zlink.zip" -DestinationPath $PWD
+Remove-Item -LiteralPath "$PWD\zlink.zip"
+Expand-Archive -Force -LiteralPath "$PWD\xpui.zip" -DestinationPath $PWD
+Remove-Item -LiteralPath "$PWD\xpui.zip"
+#>
+$spotifyInstalled = (Test-Path -LiteralPath $SpotifyExecutable)
+if (-not $spotifyInstalled) {
+  Write-Host @'
+No se detecto la instalacion nde Spotify.
+Descargando la ultima version de Spotify, Espera por favor...
+'@
+  try {
+    $webClient.DownloadFile(
+      # Remote file URL
+      'https://download.scdn.co/SpotifyFullSetup.exe',
+      # Local file path
+      "$PWD\SpotifyFullSetup.exe"
+    )
+  } catch {
+    Write-Output $_
+    Pause
+    exit
+  }
+  mkdir $SpotifyDirectory >$null 2>&1
+  Write-Host 'Ejecutando Instalacion...'
+  Start-Process -FilePath "$PWD\SpotifyFullSetup.exe"
+  Write-Host 'Deteniendo Spotify, otra vez...'
+  while ((Get-Process -name Spotify -ErrorAction SilentlyContinue) -eq $null){
+     #waiting until installation complete
+     }
+  Stop-Process -Name Spotify >$null 2>&1
+  Stop-Process -Name SpotifyWebHelper >$null 2>&1
+  Stop-Process -Name SpotifyFullSetup >$null 2>&1
+}
+
+if (!(test-path $SpotifyDirectory/chrome_elf.dll.bak)){
+	move $SpotifyDirectory\chrome_elf.dll $SpotifyDirectory\chrome_elf.dll.bak >$null 2>&1
+}
+
+Write-Host 'Parcheando Spotify...'
+$patchFiles = "$PWD\chrome_elf.dll", "$PWD\config.ini"
+<#
+$remup = "$PWD\zlink.spa"
+$uipat = "$PWD\xpui.spa"
+#>
+Copy-Item -LiteralPath $patchFiles -Destination "$SpotifyDirectory"
+<#
+$ch = Read-Host -Prompt "Optional - Remove Upgrade Button. (Y/N) "
+if ($ch -eq 'y'){
+    move $SpotifyApps\zlink.spa $SpotifyApps\zlink.spa.bak >$null 2>&1
+    Copy-Item -LiteralPath $remup -Destination "$SpotifyApps"
+} else{
+     Write-Host @'
+Won't remove Upgrade Button.
+'@`n
+}
+
+$ch = Read-Host -Prompt "Change Alpha UI back to Old UI. (BTS only supports Old UI). (Y/N) "
+if ($ch -eq 'y'){
+    move $SpotifyApps\xpui.spa $SpotifyApps\xpui.spa.bak >$null 2>&1
+    Copy-Item -LiteralPath $uipat -Destination "$SpotifyApps"
+} else{
+     Write-Host @'
+UI isn't changed.
+'@`n
+}
+#>
+
+$ch = Read-Host -Prompt "Opcional: elimine el marcador de anuncio y el botón de actualizacion a premium. (Experimental) (S/N) "
+if ($ch -eq 'y') {
+    Add-Type -Assembly 'System.IO.Compression.FileSystem'
+
+    Copy-Item -Path "$SpotifyApps\xpui.spa" -Destination "$SpotifyApps\xpui.spa.bak"
+
+    $zip = [System.IO.Compression.ZipFile]::Open("$SpotifyApps\xpui.spa", 'update')
+    $entry = $zip.GetEntry('xpui.js')
+
+    # Extract xpui.js from zip to memory
+    $reader = New-Object System.IO.StreamReader($entry.Open())
+    $xpuiContents = $reader.ReadToEnd()
+    $reader.Close()
+
+    # Replace ".ads.leaderboard.isEnabled" + separator - '}' or ')'
+    # With ".ads.leaderboard.isEnabled&&false" + separator
+    $xpuiContents = $xpuiContents -replace '(\.ads\.leaderboard\.isEnabled)(}|\))', '$1&&false$2'
+    
+    # Delete ".createElement(XX,{onClick:X,className:XX.X.UpgradeButton}),X()"
+    $xpuiContents = $xpuiContents -replace '\.createElement\([^.,{]+,{onClick:[^.,]+,className:[^.]+\.[^.]+\.UpgradeButton}\),[^.(]+\(\)', ''
+    
+    # Rewrite it to the zip
+    $writer = New-Object System.IO.StreamWriter($entry.Open())
+    $writer.BaseStream.SetLength(0)
+    $writer.Write($xpuiContents)
+    $writer.Close()
+
+    $zip.Dispose()
+} else {
+     Write-Host @'
+No eliminará el marcador de posición del anuncio ni el botón de actualización.
+'@`n
+}
+
+$tempDirectory = $PWD
+Pop-Location
+
+Remove-Item -Recurse -LiteralPath $tempDirectory  
+
+Write-Host 'Parche Completado, Iniciando Spotify...'
+Start-Process -WorkingDirectory $SpotifyDirectory -FilePath $SpotifyExecutable
+Write-Host 'Done.'
+
+write-host @'
+***************** 
+@mrpond message:
+Instagram: jaimetr_
+Github: JaimeTR
+***************** 
+'@
+
+exit
